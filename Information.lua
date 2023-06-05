30afb883f1e53bf69a9d968cd6ad48c61c171cdf
ali22001 <104569113+ali22001@users.noreply.github.com>
2023-06-05 11:28:33 +0300

    Information.lua

--- Information.lua
+++ Information.lua
@@ -1,5 +1,5 @@
 return {
-Token = "6287611476:AAGHFD0Ou_jsysesOafJ9m8WuWoYBgrHMRA",
+Token = "6287611476:AAGyFl3rLKOTcO6CZevA3QuKDm89QCrK038",
 UserBot = "RIIC_BOT",
 UserSudo = "E_M_1",
 SudoId = 5148378105}
